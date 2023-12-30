8a48b254475d6446945ea6f3fc8ffb12cff3ffae
Ebhota Jonathan <joe.ebh100@live.com>
2023-12-30 22:21:19 +0100

    Initial Setup

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+
+const urbanist = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Aurora LMS",
+  description: "Learning Management System built with NextJS",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={urbanist.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl font-medium text-sky-700 ">Aurora LMS</p>;
+      <Button>Click Me </Button>
+    </div>
+  );
+}
