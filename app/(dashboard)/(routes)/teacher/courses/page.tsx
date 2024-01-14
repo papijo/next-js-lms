e996ded93e29928f032e97fe3cea9ae67ff79d86
Ebhota Jonathan <joe.ebh100@live.com>
2024-01-14 16:57:51 +0100

    Feature: Course Title Update Form

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const CoursesPage = () => {
   return (
-    <div>
+    <div className="p-6">
       <Link href="/teacher/create">
         <Button>New Course</Button>
       </Link>
