b375219b7653a9b93b297a1046656c9a49c38f23
Ebhota Jonathan <joe.ebh100@live.com>
2024-01-23 15:33:18 +0100

    Feature: Course Data Table

--- lib/format.ts
+++ lib/format.ts
@@ -1,6 +1,6 @@
 export const formatPrice = (price: number) => {
-  return new Intl.NumberFormat("en-US", {
+  return new Intl.NumberFormat("en-NG", {
     style: "currency",
-    currency: "USD",
+    currency: "NGN",
   }).format(price);
 };
